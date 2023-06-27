d58f14fb015318d1dfab31bdd94a8d419063ff34
choijungwo <93969236+choijungwo@users.noreply.github.com>
2023-06-27 16:38:15 +0900

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -2,6 +2,7 @@ import cv2
 import pytesseract
 import json
 import re
+import numpy as np
 
 x1, y1, x2, y2 = 0, 0, 0, 0
 roi = None
@@ -40,23 +41,30 @@ def perform_ocr(image_path, region):
     #img = cv2.resize(img, (800, 200))
     
     # 이미지 반전
-    img = cv2.bitwise_not(img)
+    #img = cv2.bitwise_not(img)
     
     # 밝기 조정
-    bright = 70
+    bright = 10
     img = cv2.convertScaleAbs(img, alpha=1.0, beta=bright)
 
     # 채도 조정
-    saturation = 10.0
+    saturation = 1.0
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     img[..., 1] = img[..., 1] * saturation
     img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
     
     # 가우시안 블러 적용
-    img = cv2.GaussianBlur(img, (5, 5), 0)
+    #img = cv2.GaussianBlur(img, (5, 5), 0)
     
     # 노이즈 제거
-    img = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+    #img = cv2.fastNlMeansDenoisingColored(img, None, 10, 10, 7, 21)
+    
+    #이미지 샤프닝
+    kernel = np.array([[0, -1, 0],
+                       [-1, 6, -1],
+                       [0, -1, 0]], dtype=np.float32)
+                       
+    img = cv2.filter2D(img, -1, kernel)
     
     # 그레이스케일로 변환
     #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -64,28 +72,50 @@ def perform_ocr(image_path, region):
     # 대비 조정
     #img = cv2.equalizeHist(img)
 
-    # 곽선 검출 적용
+    # 윤곽선 검출 적용
     #img = cv2.Canny(img, 50, 150)
     
     # 이진화
     #_, img = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
     
-    # 텍스트 추출
-    text = pytesseract.image_to_string(img, config='--psm 6')
-    
-    # 추출된 텍스트 정제
-    cleaned_text = re.sub(r'\D', '', text)
-    
-    # 추출된 텍스트가 비어 있는지 확인
-    if cleaned_text == '':
-        return ocr_data
-        
     cv2.imshow('ex',img)
     cv2.waitKey(5000)
     cv2.destroyAllWindows()
     
+    # 텍스트 추출
+    text = pytesseract.image_to_string(img, config='--psm 6')
+    
+    # 한글 문자만 필터링
+    #KOR_text = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣]', '', text)
+    
+    # 영어 문자만 필터링
+    #ENG_text = re.sub(r'[^a-zA-Z]', '', text)
+    
+    # 숫자만 필터링
+    #cleaned_text = re.sub(r'\D', '', text)
+    
+    # 영어와 숫자만 필터링
+    cleaned_text = re.sub(r'\D', '', text)
+    ENG_text = re.sub(r'[^a-zA-Z]', '', text)
+    
+    # 추출된 텍스트가 비어 있는지 확인
+    if cleaned_text == '':
+        return ocr_data
+    
+    #추출(숫자만)
+    #ocr_data = {
+    #    'gas': int(cleaned_text)
+    #}
+    
+    #추출(문자만)
+    #ocr_data = {
+    #    'gas': cleaned_text.strip()
+    #}
+    
+    #추출(문자, 숫자)
     ocr_data = {
-        'gas': int(cleaned_text)
+        'english': ENG_text,
+        'num': int(cleaned_text)
     }
     
     return ocr_data
@@ -103,10 +133,10 @@ if __name__ == "__main__":
     #camera = cv2.VideoCapture(0)
     #ret, frame = camera.read()
 
-    #image_path = '/home/cjw/flaskweb/images/gasimage2.jpg'
+    #image_path = '/home/cjw/flaskweb/images/test5.jpg'
     #cv2.imwrite(image_path, frame)
 
-    image_path = '/home/cjw/flaskweb/images/gasimage2.jpg'
+    image_path = '/home/cjw/flaskweb/images/test5.jpg'
     
     image = cv2.imread(image_path)
     cv2.namedWindow("Image")
